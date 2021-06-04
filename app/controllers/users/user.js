bbaf39a8a124103f00e3845e3cee7cdba7a1f9b2
Gerrit Sommer <gerrit.sommer@outlook.com>
2021-06-04 11:57:59 +0200

    update challenge

--- app/controllers/users/user.js
+++ app/controllers/users/user.js
@@ -1,18 +1,4 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 
 export default class UsersUserController extends Controller {
-  @action
-  async modifyValue() {
-    this.model.changeValue();
-
-    // New method for changing user data, but returns an error despite working.
-    //await this.store.findRecord('user', currentUser)
-    //.then (function (user) {
-    //  user.value = !user.value;
-    //})
-    //.catch (exception => {
-    //  console.log(exception);
-    //})
-  }
 }
--- app/models/user.js
+++ app/models/user.js
@@ -3,9 +3,4 @@ import Model, { attr } from '@ember-data/model';
 export default class UserModel extends Model {
   @attr('string') name;
   @attr('string') image;
-  @attr('boolean', { defaultValue: false }) value;
-
-  changeValue() {
-    this.value = !this.value;
-  }
 }
