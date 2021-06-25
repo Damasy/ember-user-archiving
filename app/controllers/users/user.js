2a88a4bc465df189b884b27e64e646ce5fd9fe1e
Ahmed Damasy <ahmeddamasy93@gmail.com>
2021-06-25 17:41:58 +0200

    add archiving action to user controller and view

--- app/controllers/users/user.js
+++ app/controllers/users/user.js
@@ -1,3 +1,34 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+export default class UsersUserController extends Controller {
+  @tracked user;
 
-export default class UsersUserController extends Controller {}
+  get archivingMsg() {
+    if (this.model.archived) {
+      return 'User is archived..';
+    }
+    return 'User is not archived..';
+  }
+
+  @action
+  userArchive() {
+    this.model.archived = !this.model.archived;
+    this.updateDB();
+  }
+
+  updateDB() {
+    fetch('/api/users/' + this.model.id, {
+      method: 'PATCH',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify({ archived: this.model.archived }),
+    });
+  }
+}
