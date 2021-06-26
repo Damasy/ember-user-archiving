376ddc42d9504c34cf47de8987ca34166cc3d8cf
Ahmed Damasy <ahmeddamasy93@gmail.com>
2021-06-26 20:58:03 +0200

    fix 404 issue in acceptance test

--- tests/acceptance/user-archive-test.js
+++ tests/acceptance/user-archive-test.js
@@ -8,11 +8,13 @@ module('Acceptance | user archive', function(hooks) {
   test('it should view all unarchived users', async function(assert) {
     await visit('/');
 
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/', 'it is in home page');
+    assert.dom('[data-users-route]').exists();
+    // await this.pauseTest();
     await click('[data-users-route]');
-    assert.equal(currentURL(), '/users');
+    assert.equal(currentURL(), '/users', 'it is in users page');
     await click('[data-user-id="3"]');
-    assert.equal(currentURL(), '/users/3');
+    assert.equal(currentURL(), '/users/3', 'it is in Bruce Lee page');
     // await this.pauseTest();
   });
 });
