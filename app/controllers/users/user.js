ac73b1a1bd39f6a374f23dd2624264e1c8a53e30
Ahmed Damasy <ahmeddamasy93@gmail.com>
2021-06-26 16:50:03 +0200

    refactoring user controller using a service

--- app/controllers/users/user.js
+++ app/controllers/users/user.js
@@ -1,34 +1,22 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
 export default class UsersUserController extends Controller {
   @tracked user;
 
+  @service('archive') archiveService;
+
   get archivingMsg() {
-    if (this.model.archived) {
-      return 'User is archived..';
-    }
-    return 'User is not archived..';
+    return this.archiveService.getArchivingMsg(this.model.archive);
   }
 
   @action
   userArchive() {
-    this.model.archived = !this.model.archived;
-    this.updateDB();
-  }
-
-  updateDB() {
-    fetch('/api/users/' + this.model.id, {
-      method: 'PATCH',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-      body: JSON.stringify({ archived: this.model.archived }),
-    });
+    this.model.archived = this.archiveService.toggleArchive(
+      this.model.archived
+    );
+    this.archiveService.updateDB(this.model.id, this.model.archived);
   }
 }
